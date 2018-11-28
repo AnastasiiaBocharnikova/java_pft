a40bdaf2f2931c956202446323092f712e30ee02
Anastasiia Bocharnikova <anastasiia.bocharnikova@gmail.com>
2018-11-28 19:04:14 +0200

    Was added new function

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -7,23 +7,30 @@ public class MyFirstProgram {
 		hello("user");
 		hello("Nastya");
 
-		double l = 5;
-		System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+		Square s = new Square(5);
+		System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-		double a = 4;
-		double b = 6;
-		System.out.println("Площадь прямоугольника " + a + " и " + b + " = " + area(a, b));
+		Rectangle r = new Rectangle(4, 6);
+		System.out.println("Площадь прямоугольника " + r.a + " и " + r.b + " = " + r.area());
+
+
+		int x1 = 1;
+		int x2 = 3;
+		int y1 = 1;
+		int y2 = 2;
+		int x = x2 - x1;
+		int y = y2 - y1;
+		int p1 = x * x;
+		int p2 = y * y;
+		System.out.println("Расстояние между двумя точками на плоскости = " + distance(p1, p2));
 	}
 
 	public static void hello(String somebody) {
 		 System.out.println("Hello," + somebody + "!");
 	}
 
-	public static double area(double len) {
-		return len * len;
+	public static double distance(double p1, double p2) {
+		return Math.sqrt(p1 + p2);
 	}
 
-	public static double area(double a, double b) {
-		return a * b;
-	}
 }
