f5d9b639cb53a0b8259693f8abb1cdbc1fb2bc0d
Anastasiia Bocharnikova <anastasiia.bocharnikova@gmail.com>
2019-01-16 18:59:20 +0200

    16.11

--- addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -1,5 +1,8 @@
 package ru.stqa.pft.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import ru.stqa.pft.addressbook.model.ContactData;
 
 import java.io.File;
@@ -11,19 +14,35 @@ import java.util.List;
 
 public class ContactDataGenerator {
 
-    public static void main(String[] args) throws IOException {
-        int count = Integer.parseInt(args[0]);
-        File file = new File(args[1]);
+    @Parameter(names = "-c", description = "Contact count")
+    public int count;
 
+    @Parameter(names = "-f", description = "Target file")
+    public String file;
+
+    public static void main(String[] args) throws IOException {
+        ContactDataGenerator generator = new ContactDataGenerator();
+        JCommander jCommander = new JCommander(generator);
+        try {
+            jCommander.parse(args);
+        } catch (ParameterException ex) {
+            jCommander.usage();
+            return;
+        }
+        generator.run();
+    }
+
+    private void run() throws IOException {
         List<ContactData> contacts = generateContacts(count);
-        save(contacts, file);
+        save(contacts, new File(file));
     }
 
     private static void save(List<ContactData> contacts, File file) throws IOException {
         Writer writer = new FileWriter(file);
         for (ContactData contact : contacts) {
-            writer.write(String.format("%s;%s;%s;%s;%s;%s\n", contact.getFirstname(), contact.getLastname(), contact.getAddress(), contact.getEmail(),
-                    contact.getHomephone(), contact.getMobile()));
+            writer.write(String.format("%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s\n", contact.getFirstname(), contact.getMiddlename(),contact.getLastname(),
+                    contact.getNickname(), contact.getTitle(), contact.getCompany(), contact.getAddress(), contact.getHomephone(), contact.getMobile(), contact.getWorkphone(),
+                    contact.getEmail(), contact.getEmail1(), contact.getEmail2(), contact.getBday(), contact.getBmonth(), contact.getEmail(), contact.getByear(), contact.getGroup()));
         }
         writer.close();
     }
@@ -31,9 +50,10 @@ public class ContactDataGenerator {
     private static List<ContactData> generateContacts(int count) {
         List<ContactData> contacts = new ArrayList<>();
         for (int i = 0; i < count; i++) {
-            contacts.add(new ContactData().withFirstname(String.format("first name %s", i))
-                    .withLastname(String.format("last name %s", i)).withAddress(String.format("address %s", i)).withEmail(String.format("email %s", i))
-                    .withHomephone(String.format("homephone %s", i)).withMobile(String.format("mobile %s", i)));
+            contacts.add(new ContactData().withFirstname(String.format("first name %s", i)).withMiddlename(String.format("middle name %s", i)).withLastname(String.format("last name %s", i))
+                    .withNickname(String.format("nick name %s", i)).withTitle(String.format("title %s", i)).withCompany(String.format("company %s", i)).withAddress(String.format("address %s", i))
+                    .withHomephone(String.format("homephone %s", i)).withMobile(String.format("mobile %s", i)).withWorkphone(String.format("workphone %s", i)).withEmail(String.format("email %s", i))
+                    .withEmail1(String.format("email1 %s", i)).withEmail2(String.format("email2 %s", i)).withBday(String.format("bday %s", i)).withBmonth(String.format("Bmonth %s", i)).withByear(String.format("Byear %s", i)));
         }
         return contacts;
     }
--- addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,20 +1,42 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.thoughtworks.xstream.XStream;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.appmanager.TestBase;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
-  @Test
-  public void testGroupCreation() {
+  @DataProvider
+  public Iterator<Object[]> validGroups() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));
+    String xml = "";
+    String line = reader.readLine();
+    while (line != null) {
+      xml += line;
+      line = reader.readLine();
+    }
+    XStream xStream = new XStream();
+    xStream.processAnnotations(GroupData.class);
+    List<GroupData> groups = (List<GroupData>) xStream.fromXML(xml);
+    return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+  }
+
+  @Test(dataProvider = "validGroups")
+  public void testGroupCreation(GroupData group) {
     applicationManager.goTo().groupPage();
     Groups before = applicationManager.group().all();
-    GroupData group = new GroupData().withName("test12");
     applicationManager.group().create(group);
     assertThat(applicationManager.group().count(), equalTo(before.size() + 1));
     Groups after = applicationManager.group().all();
